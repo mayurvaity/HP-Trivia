9993be51b11685182cdd66c736501f3b767eaad3
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-07-16 23:08:46 +0530

    Initial Commit

---
+++ HP Trivia-SwiftUI-iOS17/HP_Trivia_SwiftUI_iOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  HP_Trivia_SwiftUI_iOS17App.swift
+//  HP Trivia-SwiftUI-iOS17
+//
+//  Created by Mayur Vaity on 16/07/24.
+//
+
+import SwiftUI
+
+@main
+struct HP_Trivia_SwiftUI_iOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
