377c0a94b5b156c15347d7bda4057738c7a9d0f4
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-07-17 01:48:55 +0530

    Lay out the Title Screen.

---
+++ HP Trivia-SwiftUI-iOS17/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  HP Trivia-SwiftUI-iOS17
+//
+//  Created by Mayur Vaity on 17/07/24.
+//
+
+import Foundation
+
+enum Constants {
+    static let hpFont = "PartyLetPlain"
+}
--- HP Trivia-SwiftUI-iOS17/ContentView.swift
+++ HP Trivia-SwiftUI-iOS17/ContentView.swift
@@ -9,13 +9,98 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geo in
+            ZStack {
+                //bg image
+                Image("hogwarts")
+                    .resizable()
+                    .frame(width: geo.size.width * 3, height: geo.size.height)
+                    .padding(.top, 3) //to push the image down a little
+                
+                VStack {
+                    VStack {
+                        //bolt image
+                        Image(systemName: "bolt.fill")
+                            .font(.largeTitle)
+                            .imageScale(.large)
+                        
+                        Text("HP")
+                            .font(.custom(Constants.hpFont, size: 70))
+                            .padding(.bottom, -50) //to bring HP and Trivia words closer
+                        
+                        Text("Trivia")
+                            .font(.custom(Constants.hpFont, size: 60))
+                    }
+                    .padding(.top, 70)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("Recent Scores")
+                            .font(.title2)
+                        
+                        Text("33")
+                        Text("27")
+                        Text("15")
+                    }
+                    .font(.title3)
+                    .padding(.horizontal)
+                    .foregroundStyle(.white)
+                    .background(.black.opacity(0.7))
+                    .clipShape(.rect(cornerRadius: 15))
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button {
+                            //show instructions screen
+                        } label: {
+                            Image(systemName: "info.circle.fill")
+                                .font(.largeTitle)
+                                .foregroundStyle(.white)
+                                .shadow(radius: 5)
+                        }
+                        
+                        Spacer()
+                        
+                        Button {
+                            //start a new game
+                        } label: {
+                            Text("Play")
+                                .font(.largeTitle)
+                                .foregroundStyle(.white)
+                                .padding(.vertical, 7)
+                                .padding(.horizontal, 50)
+                                .background(.brown)
+                                .clipShape(.rect(cornerRadius: 7))
+                                .shadow(radius: 5)
+                        }
+                        
+                        
+                        Spacer()
+                        
+                        Button {
+                            //show settings screen
+                        } label: {
+                            Image(systemName: "gearshape.fill")
+                                .font(.largeTitle)
+                                .foregroundStyle(.white)
+                                .shadow(radius: 5)
+                        }
+                        
+                        Spacer()
+                    }
+                    .frame(width: geo.size.width)
+                    
+                    Spacer()
+                }
+            }
+            .frame(width: geo.size.width,
+                   height: geo.size.height)
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
