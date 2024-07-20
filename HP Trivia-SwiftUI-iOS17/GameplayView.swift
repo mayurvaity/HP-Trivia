72d46e2a6e90ca5d608db0cf21dd32842d468476
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-07-21 02:48:26 +0530

    Implement Celebration Screen.

--- HP Trivia-SwiftUI-iOS17/GameplayView.swift
+++ HP Trivia-SwiftUI-iOS17/GameplayView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct GameplayView: View {
     //var to handle animations
     @State private var animateViewsIn = false
+    //var used to trigger celebration screen
+    @State private var tappedCorrectAnswer = false
     
     var body: some View {
         GeometryReader { geo in
@@ -115,12 +117,74 @@ struct GameplayView: View {
                 .frame(width: geo.size.width, height: geo.size.height)
                 .foregroundStyle(.white)
                 
+                //MARK: - Celebration screen
+                VStack {
+                    Spacer()
+                    
+                    VStack {
+                        if tappedCorrectAnswer {
+                            Text("5")
+                                .font(.largeTitle)
+                                .padding(.top, 50)
+                                .transition(.offset(y: -geo.size.height/4))
+                        }
+                    }
+                    .animation(.easeInOut(duration: 1).delay(2), value: tappedCorrectAnswer)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        if tappedCorrectAnswer {
+                            Text("Brilliant!")
+                                .font(.custom(Constants.hpFont, size: 100))
+                                .transition(.scale.combined(with: .offset(y: -geo.size.height/2))) // combined - to combine 2 diff types of animations together on this view
+                        }
+                    }
+                    .animation(.easeInOut(duration: 1).delay(1), value: tappedCorrectAnswer)
+                    
+                    Spacer()
+                    
+                    Text("Answer 1")
+                        .minimumScaleFactor(0.5) //to specify minimum font size
+                        .multilineTextAlignment(.center) //text alignment when going multiline
+                        .padding(10)
+                        .frame(width: geo.size.width/2.15, height: 80)
+                        .background(.green.opacity(0.5))
+                        .clipShape(.rect(cornerRadius: 25))
+                        .scaleEffect(2)
+                    
+                    Group {
+                        Spacer()
+                        Spacer()
+                    }
+                    
+                    VStack {
+                        if tappedCorrectAnswer {
+                            Button("Next Level>") {
+                                // TODO:
+                            }
+                            .buttonStyle(.borderedProminent)
+                            .tint(.blue.opacity(0.5))
+                            .font(.largeTitle)
+                            .transition(.offset(y: geo.size.height/3))
+                        }
+                    }
+                    .animation(.easeInOut(duration: 2.7).delay(2.7), value: tappedCorrectAnswer)
+                    
+                    Group {
+                        Spacer()
+                        Spacer()
+                    }
+                }
+                .foregroundStyle(.white)
+                
             }
             .frame(width: geo.size.width, height: geo.size.height) //this sets size of zstack and also aligns everything within in center
         }
         .ignoresSafeArea()
         .onAppear {
-            animateViewsIn = true
+//            animateViewsIn = true
+            tappedCorrectAnswer = true
         }
     }
 }
