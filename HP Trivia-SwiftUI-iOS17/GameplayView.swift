89e8748a8daba0fae6fbebeb83ce7e2fcf964cbf
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-07-21 02:22:16 +0530

    Implement Gameplay Transitions.

--- HP Trivia-SwiftUI-iOS17/GameplayView.swift
+++ HP Trivia-SwiftUI-iOS17/GameplayView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct GameplayView: View {
+    //var to handle animations
+    @State private var animateViewsIn = false
+    
     var body: some View {
         GeometryReader { geo in
             ZStack {
@@ -18,6 +21,7 @@ struct GameplayView: View {
                     .overlay(Rectangle().foregroundStyle(.black.opacity(0.8)))
                 
                 VStack {
+                    //MARK: - Controls
                     HStack {
                         Button("End Game") {
                             //TODO: End game
@@ -32,51 +36,77 @@ struct GameplayView: View {
                     .padding()
                     .padding(.vertical, 30)
                     
-                    //question
-                    Text("Who is Harry Potter?")
-                        .font(.custom(Constants.hpFont, size: 50))
-                        .multilineTextAlignment(.center)
-                        .padding()
+                    //MARK: - Question
+                    VStack {
+                        if animateViewsIn {
+                            Text("Who is Harry Potter?")
+                                .font(.custom(Constants.hpFont, size: 50))
+                                .multilineTextAlignment(.center)
+                                .padding()
+                                .transition(.scale)  //transition type for animation
+                        }
+                    }
+                    .animation(.easeIn(duration: 2), value: animateViewsIn) //animation specified with var it depends on
                     
                     Spacer()
                     
+                    //MARK: - Hints 
                     HStack {
-                        Image(systemName: "questionmark.app.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100)
-                            .foregroundStyle(.cyan)
-                            .rotationEffect(.degrees(-15)) //to rotate image by angle in degrees
-                            .padding()
-                            .padding(.leading, 20)
+                        VStack {
+                            if animateViewsIn {
+                                Image(systemName: "questionmark.app.fill")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100)
+                                    .foregroundStyle(.cyan)
+                                    .rotationEffect(.degrees(-15)) //to rotate image by angle in degrees
+                                    .padding()
+                                    .padding(.leading, 20)
+                                    .transition(.offset(x: -geo.size.width/2))
+                            }
+                        }
+                        .animation(.easeOut(duration: 1.5).delay(2), value: animateViewsIn)
                         
                         Spacer()
                         
-                        Image(systemName: "book.closed")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 50)
-                            .foregroundStyle(.black)
-                            .frame(width: 100, height: 100)
-                            .background(.cyan)
-                            .clipShape(.rect(cornerRadius: 20))
-                            .rotationEffect(.degrees(15))
-                            .padding()
-                            .padding(.trailing, 20)
+                        VStack {
+                            if animateViewsIn {
+                                Image(systemName: "book.closed")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50)
+                                    .foregroundStyle(.black)
+                                    .frame(width: 100, height: 100)
+                                    .background(.cyan)
+                                    .clipShape(.rect(cornerRadius: 20))
+                                    .rotationEffect(.degrees(15))
+                                    .padding()
+                                    .padding(.trailing, 20)
+                                    .transition(.offset(x: geo.size.width/2))
+                            }
+                        }
+                        .animation(.easeOut(duration: 1.5).delay(2), value: animateViewsIn)
                         
                     }
                     .padding(.bottom)
                     
+                    //MARK: - Answers
                     LazyVGrid(columns: [GridItem(), GridItem()]) {
                         ForEach(1..<5) {
                             i in
-                            Text(i == 3 ? "The boy who basically lived and got sent to his relatives house where he was treated quite badly if I'm being honest but yeah." : "Answer \(i)")
-                                .minimumScaleFactor(0.5) //to specify minimum font size
-                                .multilineTextAlignment(.center)
-                                .padding(10)
-                                .frame(width: geo.size.width/2.15, height: 80)
-                                .background(.green.opacity(0.5))
-                                .clipShape(.rect(cornerRadius: 25))
+                            VStack {
+                                if animateViewsIn {
+                                    Text(i == 3 ? "The boy who basically lived and got sent to his relatives house where he was treated quite badly if I'm being honest but yeah." : "Answer \(i)")
+                                        .minimumScaleFactor(0.5) //to specify minimum font size
+                                        .multilineTextAlignment(.center)
+                                        .padding(10)
+                                        .frame(width: geo.size.width/2.15, height: 80)
+                                        .background(.green.opacity(0.5))
+                                        .clipShape(.rect(cornerRadius: 25))
+                                        .transition(.scale)
+                                }
+                            }
+                            .animation(.easeOut(duration: 1).delay(1.5), value: animateViewsIn)
                         }
                     }
                     
@@ -89,6 +119,9 @@ struct GameplayView: View {
             .frame(width: geo.size.width, height: geo.size.height) //this sets size of zstack and also aligns everything within in center
         }
         .ignoresSafeArea()
+        .onAppear {
+            animateViewsIn = true
+        }
     }
 }
 
